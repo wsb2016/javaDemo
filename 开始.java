9d2ab2baf997cdeaf6160008c6896a40fd5babcf
wsb2016 <980213669@qq.com>
2021-03-18 18:00:40 +0800

    Add files via upload

---
+++ 开始.java
@@ -0,0 +1,16 @@
+public class 开始 {
+    public static void main(String[] args) {
+//new test1();
+//new 改后缀名();
+           执行CMD命令 c= new 执行CMD命令();
+                   c.列出文件清单();
+//                   c.执行命令();
+//        new 批量改名任务();
+//    new 查找文件夹或文件().查找("hls目录","z:/");
+//        new 连接两个文件().按String类连接();
+
+
+    }
+
+
+}
